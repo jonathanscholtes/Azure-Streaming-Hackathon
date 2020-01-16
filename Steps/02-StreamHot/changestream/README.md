45c08fd8c62afd197f05f3d1319390c2104837d5
Jonathan Scholtes <joscholt@microsoft.com>
2020-01-16 01:51:52 -0600

    fixed container deploy

--- Steps/02-StreamHot/changestream/README.md
+++ Steps/02-StreamHot/changestream/README.md
@@ -1,3 +1,15 @@
+# Change streams in Azure Cosmos DB’s API for MongoDB
 
+Change feed support in Azure Cosmos DB’s API for MongoDB is available by using the change streams API.
 
-az container create -g MyResourceGroup --name myapp --image myimage:latest --cpu 1 --memory 1
+mongo DB 3.6 is supported, not 3.2.
+
+Change Stream Container code is located in the [python](python/) directory.
+
+Docker Containers have been pushed to [jscholtes/cosmosdb.changestream.python](https://hub.docker.com/repository/docker/jscholtes/cosmosdb.changestream.python/general)
+
+## Create Azure Container Instance using Docker Image
+
+```bash
+az container create -g MyResourceGroup --name myapp --image jscholtes/cosmosdb.changestream.python:v1 --cpu 1 --memory 1 --environment-variables EVENT_HUB_CONNECTION_STR='<>' EVENT_HUB_NAME='<>' COSMOS_ACCOUNT_NAME='<>' COSMOS_PASSWORD='<>' COSMOS_DATABASE='<>' COSMOS_COLLECTION='<>'
+```
--- Steps/02-StreamHot/changestream/python/monitor_mongo.py
+++ Steps/02-StreamHot/changestream/python/monitor_mongo.py
@@ -1,24 +1,30 @@
 from pymongo import MongoClient
 from azure.eventhub import EventHubProducerClient, EventData
 import json
+import os
 
-connection_str = '<connection string>
-eventhub_name = '<hub>'
+
+connection_str = os.environ['EVENT_HUB_CONNECTION_STR']
+eventhub_name = os.environ['EVENT_HUB_NAME']
+cosmos_account = os.environ['COSMOS_ACCOUNT_NAME']
+cosmos_pass = os.environ['COSMOS_PASSWORD']
+database= os.environ['COSMOS_DATABASE']
+collection= os.environ['COSMOS_COLLECTION']
+
 producer_client = EventHubProducerClient.from_connection_string(connection_str, eventhub_name=eventhub_name)
 
-
-client = MongoClient('mongodb://<Cosmos Account>.mongo.cosmos.azure.com:10255/?ssl=true')
+client = MongoClient('mongodb://{}.mongo.cosmos.azure.com:10255/?ssl=true'.format(cosmos_account))
         
-db = client['<Database>']
+db = client[database]
     
-db.authenticate(name="<Cosmos Account>",password='<Cosmos Password>')
+db.authenticate(name=cosmos_account,password=cosmos_pass)
 
 pipeline = [{ '$match': { 'operationType': { '$in': ['insert', 'update', 'replace'] } } },
         { '$project': { '_id': 1, 'fullDocument': 1, 'ns': 1, 'documentKey': 1 } }]
 
 #option = {'fullDocument':'updateLookup'}
 
-change_stream = db['<collection>'].watch(pipeline,'updateLookup')
+change_stream = db[collection].watch(pipeline,'updateLookup')
 
 for change in change_stream:
     jsn = str(change)
